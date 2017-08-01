6508088e7d17d1be79b77bf63169599ac512b4f3
a152089X <a152089X@L2-02.hto.ifsp.edu.br>
2017-08-01 20:25:11 -0300

    Mais uma parte

--- POO/src/helloAdele.java
+++ POO/src/helloAdele.java
@@ -2,7 +2,8 @@
 public class helloAdele {
 
 	public static void main(String[] args){
-			System.out.print("Hello, it's meee...");
+		System.out.print("Hello, it's meee...");
+		System.out.print("I'm in California");
 			
 		
 	}
