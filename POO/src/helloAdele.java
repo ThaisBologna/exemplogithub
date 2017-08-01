16ccc322a45ce96f514cb305af7b66ea3391e329
a152089X <a152089X@L2-02.hto.ifsp.edu.br>
2017-08-01 20:27:13 -0300

    Para comparar

--- POO/src/helloAdele.java
+++ POO/src/helloAdele.java
@@ -4,6 +4,7 @@ public class helloAdele {
 	public static void main(String[] args){
 		System.out.print("Hello, it's meee...");
 		System.out.print("I'm in California");
+		System.out.print("Or no");
 			
 		
 	}
